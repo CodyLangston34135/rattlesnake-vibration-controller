6fc7d53dea978527aec75db9b29974b87c0c62ae
Chad Hovey <hovey@me.com>
2025-08-12 14:24:53 -0400

    consolidate utilities, python for main report too

--- src/rattlesnake/cicd/utilities.py
+++ src/rattlesnake/cicd/utilities.py
@@ -3,6 +3,10 @@
 Utilities for CICD processes.
 """
 
+from datetime import datetime
+import pytz
+
+
 def get_score_color(pylint_score: str) -> str:
     """
     Determine color based on pylint score.
@@ -25,3 +29,33 @@ def get_score_color(pylint_score: str) -> str:
             return "red"
     except ValueError:
         return "gray"
+
+
+def get_timestamp() -> str:
+    """
+    Get formatted timestamp with UTC, EST, and MST times.
+
+    Returns:
+        Formatted timestamp string
+    """
+    # Get the current UTC time
+    utc_now: datetime = datetime.now(pytz.utc)
+
+    # Define the time zones
+    timezone_est: pytz.BaseTzInfo = pytz.timezone("America/New_York")
+    timezone_mst: pytz.BaseTzInfo = pytz.timezone("America/Denver")
+
+    # Convert UTC time to EST and MST
+    est_now: datetime = utc_now.astimezone(timezone_est)
+    mst_now: datetime = utc_now.astimezone(timezone_mst)
+
+    # Format the output
+    df: str = "%Y-%m-%d %H:%M:%S "  # Date format
+    utc: str = utc_now.strftime(df + "UTC")
+    est: str = est_now.strftime(df + "EST")
+    mst: str = mst_now.strftime(df + "MST")
+
+    # Combine the formatted times
+    timestamp: str = f"{utc} ({est} / {mst})"
+
+    return timestamp
