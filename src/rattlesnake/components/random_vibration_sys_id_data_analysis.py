6f3298987dfb816007d603040a77d3f4a2bde9e6
Dan Rohe <dprohe@sandia.gov>
2025-12-22 10:17:21 -0700

    Fixed problems identified by pylint and flake8

--- src/rattlesnake/components/random_vibration_sys_id_data_analysis.py
+++ src/rattlesnake/components/random_vibration_sys_id_data_analysis.py
@@ -21,32 +21,35 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+import importlib
+import multiprocessing as mp
+import os
+import time
+from enum import Enum
+
+import numpy as np
+
 from .abstract_sysid_data_analysis import (
     AbstractSysIDAnalysisProcess,
     SysIDDataAnalysisCommands,
 )
 from .random_vibration_sys_id_environment import (
-    RandomVibrationMetadata,
     RandomVibrationCommands,
+    RandomVibrationMetadata,
 )
 from .utilities import (
-    flush_queue,
     GlobalCommands,
     VerboseMessageQueue,
+    flush_queue,
+    power2db,
     rms_csd,
     rms_time,
-    power2db,
 )
-import multiprocessing as mp
-import numpy as np
-from enum import Enum
-import os
-import importlib
-import time
-import sys
 
 
 class RandomVibrationDataAnalysisCommands(Enum):
+    """Enumeration containing valid commands for the random data analysis process"""
+
     INITIALIZE_PARAMETERS = 0
     PERFORM_CONTROL_PREDICTION = 1
     RUN_CONTROL = 2
@@ -56,6 +59,7 @@ class RandomVibrationDataAnalysisCommands(Enum):
 
 
 class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
+    """Control calculations for the Random Vibration environment"""
 
     def __init__(
         self,
@@ -86,19 +90,13 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
             RandomVibrationDataAnalysisCommands.PERFORM_CONTROL_PREDICTION,
             self.perform_control_prediction,
         )
-        self.map_command(
-            RandomVibrationDataAnalysisCommands.RUN_CONTROL, self.run_control
-        )
-        self.map_command(
-            RandomVibrationDataAnalysisCommands.STOP_CONTROL, self.stop_control
-        )
+        self.map_command(RandomVibrationDataAnalysisCommands.RUN_CONTROL, self.run_control)
+        self.map_command(RandomVibrationDataAnalysisCommands.STOP_CONTROL, self.stop_control)
         self.map_command(
             GlobalCommands.UPDATE_INTERACTIVE_CONTROL_PARAMETERS,
             self.update_interactive_control_parameters,
         )
-        self.map_command(
-            GlobalCommands.SEND_INTERACTIVE_COMMAND, self.send_interactive_command
-        )
+        self.map_command(GlobalCommands.SEND_INTERACTIVE_COMMAND, self.send_interactive_command)
         self.error_indices = None
         self.control_function = None
         self.response_cpsd_prediction = None
@@ -120,16 +118,13 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
         # print(type(data.specification_cpsd_matrix))
         # print(data.specification_cpsd_matrix)
         self.error_indices = ~np.all(
-            (data.specification_cpsd_matrix == 0)
-            | np.isnan(data.specification_cpsd_matrix),
+            (data.specification_cpsd_matrix == 0) | np.isnan(data.specification_cpsd_matrix),
             axis=(-1, -2),
         )
-        self.frequencies = self.parameters.frequency_spacing * np.arange(
-            self.parameters.fft_lines
-        )
+        self.frequencies = self.parameters.frequency_spacing * np.arange(self.parameters.fft_lines)
         # Load in the control script
-        path, file = os.path.split(data.control_python_script)
-        file, ext = os.path.splitext(file)
+        _, file = os.path.split(data.control_python_script)
+        file, _ = os.path.splitext(file)
         spec = importlib.util.spec_from_file_location(file, data.control_python_script)
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
@@ -143,9 +138,7 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
             self.control_function = generator_function.send
         elif self.parameters.control_python_function_type == 2:  # Class
             control_frf = (
-                self.control_frf
-                if self.parameters.update_tf_during_control
-                else self.sysid_frf
+                self.control_frf if self.parameters.update_tf_during_control else self.sysid_frf
             )
             control_coherence = (
                 self.control_coherence
@@ -170,9 +163,7 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
             )
         elif self.parameters.control_python_function_type == 3:  # Interactive
             control_frf = (
-                self.control_frf
-                if self.parameters.update_tf_during_control
-                else self.sysid_frf
+                self.control_frf if self.parameters.update_tf_during_control else self.sysid_frf
             )
             control_coherence = (
                 self.control_coherence
@@ -203,7 +194,8 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
         else:  # Function
             self.control_function = getattr(module, data.control_python_function)
 
-    def perform_control_prediction(self, data):
+    def perform_control_prediction(self, data):  # pylint: disable=unused-argument
+        """Runs the control law with system identification information to predict control"""
         if self.sysid_frf is None:
             return
         if self.parameters.control_python_function_type == 1:  # Generator
@@ -220,7 +212,7 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
                     self.sysid_coherence,  # Coherence from the system identification
                     self.parameters.sysid_averages,  # Number of frames in the CPSD
                     self.parameters.sysid_averages,  # Total number of frames
-                    self.parameters.control_python_function_parameters,  # Extra parameters for the control law
+                    self.parameters.control_python_function_parameters,  # Extra parameters
                     None,  # Last Control Response for Error Correction
                     None,  # Last Control Excitation for Drive-based control
                 )
@@ -255,7 +247,7 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
                                     self.sysid_reference_noise,  # from the system identification
                                     self.sysid_response_cpsd,  # Response levels and correlation
                                     self.sysid_reference_cpsd,  # from the system identification
-                                    self.sysid_coherence,  # Coherence from the system identification
+                                    self.sysid_coherence,  # Coherence from the system id
                                 ),
                             ),
                         )
@@ -274,9 +266,7 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
                     None,
                 )
             else:
-                self.log(
-                    "Have not yet received control parameters from interactive control law!"
-                )
+                self.log("Have not yet received control parameters from interactive control law!")
                 self.command_queue.put(
                     self.process_name,
                     (
@@ -299,24 +289,18 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
                 self.sysid_coherence,  # Coherence from the system identification
                 self.parameters.sysid_averages,  # Number of frames in the CPSD
                 self.parameters.sysid_averages,  # Total number of frames
-                self.parameters.control_python_function_parameters,  # Extra parameters for the control law
+                self.parameters.control_python_function_parameters,  # Extra parameters
                 None,  # Last Control Response for Error Correction
                 None,  # Last Control Excitation for Drive-based control
             )
-        response_cpsd = (
-            self.sysid_frf @ output_cpsd @ self.sysid_frf.conjugate().transpose(0, 2, 1)
-        )
+        response_cpsd = self.sysid_frf @ output_cpsd @ self.sysid_frf.conjugate().transpose(0, 2, 1)
         self.drive_cpsd_prediction = output_cpsd
         self.response_cpsd_prediction = response_cpsd
-        rms_drives = rms_csd(
-            self.drive_cpsd_prediction, self.parameters.frequency_spacing
-        )
+        rms_drives = rms_csd(self.drive_cpsd_prediction, self.parameters.frequency_spacing)
         response_db_error = power2db(
             np.einsum("ijj->ij", self.response_cpsd_prediction[self.error_indices]).real
         ) - power2db(
-            np.einsum(
-                "ijj->ij", self.parameters.specification_cpsd_matrix[self.error_indices]
-            ).real
+            np.einsum("ijj->ij", self.parameters.specification_cpsd_matrix[self.error_indices]).real
         )
         rms_db_error = rms_time(response_db_error, axis=0)
         self.gui_update_queue.put(
@@ -338,7 +322,8 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
         if self.parameters.control_python_function_type == 3:
             self.has_sent_interactive_control_transfer_function_results = False
 
-    def run_control(self, data):
+    def run_control(self, data):  # pylint: disable=unused-argument
+        """Runs the control law to generate new output CPSDs"""
         if self.startup:
             self.log("Starting Control")
             self.frames = 0
@@ -378,9 +363,7 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
             warning_channels = []
             abort_channels = []
             with np.errstate(invalid="ignore"):
-                lines_out = (
-                    self.parameters.percent_lines_out / 100
-                ) * self.parameters.fft_lines
+                lines_out = (self.parameters.percent_lines_out / 100) * self.parameters.fft_lines
                 for i in range(self.last_response_cpsd.shape[-1]):
                     if (
                         sum(
@@ -419,8 +402,8 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
                 and self.frames == self.parameters.frames_in_cpsd
                 and self.parameters.allow_automatic_aborts
             ):
-                print("Aborting due to channel indices {:}".format(abort_channels))
-                self.log("Aborting due to channel indices {:}".format(abort_channels))
+                print(f"Aborting due to channel indices {abort_channels}")
+                self.log(f"Aborting due to channel indices {abort_channels}")
                 self.environment_command_queue.put(
                     self.process_name, (RandomVibrationCommands.STOP_CONTROL, None)
                 )
@@ -445,9 +428,7 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
             self.log("Controlling")
             # Create the new control output
             control_frf = (
-                self.control_frf
-                if self.parameters.update_tf_during_control
-                else self.sysid_frf
+                self.control_frf if self.parameters.update_tf_during_control else self.sysid_frf
             )
             control_coherence = (
                 self.control_coherence
@@ -468,7 +449,7 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
                         control_coherence,  # Coherence
                         self.frames,  # Number of frames in the CPSD
                         self.parameters.frames_in_cpsd,  # Total number of frames
-                        self.parameters.control_python_function_parameters,  # Extra parameters for the control law
+                        self.parameters.control_python_function_parameters,  # Extra parameters
                         self.last_response_cpsd,  # Last Control Response for Error Correction
                         self.last_drive_cpsd,  # Last Control Excitation for Drive-based control
                     )
@@ -498,14 +479,13 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
                     control_coherence,  # Coherence
                     self.frames,  # Number of frames in the CPSD
                     self.parameters.frames_in_cpsd,  # Total number of frames
-                    self.parameters.control_python_function_parameters,  # Extra parameters for the control law
+                    self.parameters.control_python_function_parameters,  # Extra parameters
                     self.last_response_cpsd,  # Last Control Response for Error Correction
                     self.last_drive_cpsd,  # Last Control Excitation for Drive-based control
                 )
             self.log(
-                "RMS Outputs from Control \n  {:}".format(
-                    rms_csd(output_cpsd, self.parameters.frequency_spacing)
-                )
+                f"RMS Outputs from Control \n  "
+                f"{rms_csd(output_cpsd, self.parameters.frequency_spacing)}"
             )
             self.data_out_queue.put([output_cpsd])
             self.log("Finished Controlling")
@@ -518,23 +498,28 @@ class RandomVibrationDataAnalysisProcess(AbstractSysIDAnalysisProcess):
         )
 
     def update_interactive_control_parameters(self, interactive_control_parameters):
+        """Updates parameters for the interactive control law"""
         if self.parameters.control_python_function_type == 3:  # Interactive
             self.control_function.update_parameters(interactive_control_parameters)
             self.last_interactive_parameters = interactive_control_parameters
         else:
             raise ValueError(
-                "Received an UPDATE_INTERACTIVE_CONTROL_PARAMETERS signal without an interactive control law.  How did this happen?"
+                "Received an UPDATE_INTERACTIVE_CONTROL_PARAMETERS signal without an "
+                "interactive control law.  How did this happen?"
             )
 
     def send_interactive_command(self, command):
+        """Sends a command to the interactive control law"""
         if self.parameters.control_python_function_type == 3:  # Interactive
             self.control_function.send_command(command)
         else:
             raise ValueError(
-                "Received an UPDATE_INTERACTIVE_CONTROL_PARAMETERS signal without an interactive control law.  How did this happen?"
+                "Received an UPDATE_INTERACTIVE_CONTROL_PARAMETERS signal without an "
+                "interactive control law.  How did this happen?"
             )
 
-    def stop_control(self, data):
+    def stop_control(self, data):  # pylint: disable=unused-argument
+        """Stops the data acquisition process"""
         # Remove any run_transfer_function or run_control from the queue
         instructions = self.command_queue.flush(self.process_name)
         for instruction in instructions:
@@ -562,6 +547,28 @@ def random_data_analysis_process(
     log_file_queue: mp.queues.Queue,
     process_name=None,
 ):
+    """Process defining the random vibration control calculations and data analysis
+
+    Parameters
+    ----------
+    environment_name : str
+        The name of the random vibration environment
+    command_queue : VerboseMessageQueue
+        A message queue that will provide commands to this process
+    data_in_queue : mp.queues.Queue
+        A queue from which data will be received to use for control updates
+    data_out_queue : mp.queues.Queue
+        A queue into which the results of control calculations will be placed
+    environment_command_queue : VerboseMessageQueue
+        A message queue to send commands back to the main controller
+    gui_update_queue : mp.queues.Queue
+        A queue to send updates to the graphical user interface for updating interactive control
+        laws
+    log_file_queue : mp.queues.Queue
+        A queue to send messages that will be logged in the log file
+    process_name : str, optional
+        A name for the process.  If not specified, it will be
+    """
     data_analysis_instance = RandomVibrationDataAnalysisProcess(
         environment_name + " Data Analysis" if process_name is None else process_name,
         command_queue,
