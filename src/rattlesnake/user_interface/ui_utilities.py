1f7b64dd276fd757e7a5a623707f7dc990769a69
CodyLangston34135 <Cody.Langston75@gmail.com>
2026-01-26 16:46:04 -0500

    Started implementation of GUI that can plot data from gui_update_queue

---
+++ src/rattlesnake/user_interface/ui_utilities.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from enum import Enum
+
+
+class UICommands:
+    ENABLE = 0
+    DISABLE = 1
+    DATA = 2
+
+
+class DebugPlot:
+    def __init__(self, gui_update_queue):
+        pass
