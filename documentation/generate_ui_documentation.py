4d3cc55ecc4e278660fc43a509776b690724f0d2
Dan Rohe <dprohe@sandia.gov>
2026-02-16 14:29:56 -0700

    Updated auto-documentation of user interface to build figures and references into the jupyter book.

--- documentation/generate_ui_documentation.py
+++ documentation/generate_ui_documentation.py
@@ -27,8 +27,17 @@ import os
 import re
 import sys
 from qtpy import QtWidgets, uic
+from qtpy.QtCore import QRect
+from qtpy.QtGui import QPixmap, QPainter, QPen, QColor
+import pyqtgraph
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+pyqtgraph.setConfigOption("background", "w")
+pyqtgraph.setConfigOption("foreground", "k")
+
+try:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+except NameError:
+    dir_path = "."
 
 files = [
     dir_path + "/" + v
@@ -37,10 +46,6 @@ files = [
         "../src/rattlesnake/components/modal_run.ui",
         "../src/rattlesnake/components/modal_definition.ui",
         "../src/rattlesnake/components/modal_acquisition_window.ui",
-        # "../src/rattlesnake/components/ip_manager.ui",
-        # "../src/rattlesnake/components/environment_selector.ui",
-        # "../src/rattlesnake/components/combined_environments_controller.ui",
-        # "../src/rattlesnake/components/control_select.ui",
         "../src/rattlesnake/components/transient_run.ui",
         "../src/rattlesnake/components/transient_prediction.ui",
         "../src/rattlesnake/components/transient_definition.ui",
@@ -59,6 +64,11 @@ files = [
 ]
 
 
+# "../src/rattlesnake/components/ip_manager.ui",
+# "../src/rattlesnake/components/environment_selector.ui",
+# "../src/rattlesnake/components/combined_environments_controller.ui",
+# "../src/rattlesnake/components/control_select.ui",
+
 class UIAnalyzer(QtWidgets.QMainWindow):
     """A Class to analyze the contents of a .ui file and create a markdown file documenting it
     """
@@ -72,11 +82,12 @@ class UIAnalyzer(QtWidgets.QMainWindow):
             Path to a .ui file to analyze and document
         """
         super().__init__()
+        self.name = os.path.splitext(os.path.split(ui_file)[-1])[0]
         self.load_ui(ui_file)
         self.print_depth = 0
         self.all_widgets = None
         self.all_layouts = None
-        self.currently_in_groupboxes = []
+        self.resize(1800, 1000)
 
     def load_ui(self, ui_file):
         """Loads in a ui file and shows it in a main window
@@ -124,10 +135,15 @@ class UIAnalyzer(QtWidgets.QMainWindow):
             name = type(item).__name__
         print(f"Analyzing {name}")
         try:
-            position = item.mapToGlobal(item.rect().topLeft())
+            item_rect = item.rect()
+            position = item.mapToGlobal(item_rect.topLeft())
+            height = item_rect.height()
+            width = item_rect.width()
+            box = [position.x(), position.y(), width, height]
             position = [position.x(), position.y()]
         except AttributeError:
             position = None
+            box = None
         try:
             tool_tip = item.toolTip()
         except AttributeError:
@@ -137,6 +153,7 @@ class UIAnalyzer(QtWidgets.QMainWindow):
             "type": type(item),
             "tooltip": tool_tip,
             "pos": position,
+            "box": box,
             "children": [],
             "widget": item,
         }
@@ -208,6 +225,31 @@ class UIAnalyzer(QtWidgets.QMainWindow):
 
         return structure
 
+    def reduced_structure(self, full_structure=None, structure_dictionary=None):
+        if full_structure is None:
+            full_structure = self.export_structure()
+        if structure_dictionary is None:
+            structure_dictionary = {}
+
+        if isinstance(full_structure["widget"], QtWidgets.QGroupBox):
+            structure_dictionary[full_structure["widget"].title()] = full_structure.copy()
+            del structure_dictionary[full_structure["widget"].title()]["children"]
+            structure_dictionary[full_structure["widget"].title()]["children"] = {}
+            children_dictionary = structure_dictionary[full_structure["widget"].title()]["children"]
+        else:
+            children_dictionary = structure_dictionary
+
+        if full_structure["tooltip"] is not None and full_structure["tooltip"].strip() != "":
+            label, message = self.parse_tooltip(full_structure["tooltip"])
+            structure_dictionary[label] = full_structure.copy()
+            del structure_dictionary[label]["children"]
+
+        # Now go analyze the children
+        for child in full_structure["children"]:
+            self.reduced_structure(child, children_dictionary)
+
+        return structure_dictionary
+
     def print_structure(self):
         """Prints a representation of the ui hierarchy"""
         struct = self.export_structure()
@@ -223,6 +265,26 @@ class UIAnalyzer(QtWidgets.QMainWindow):
         self.print_depth += 1
         for child in children:
             self._print_struct_item(child)
+        self.print_depth -= 1
+
+    def print_reduced_structure(self):
+        reduced_structure = self.reduced_structure()
+        for name, data in reduced_structure.items():
+            self._print_reduced_struct_item(name, data)
+
+    def _print_reduced_struct_item(self, key, data):
+        name = data["name"]
+        position = data["pos"]
+        # tooltip = struct['tooltip']
+        typ = data["type"]
+        try:
+            children = data["children"]
+        except KeyError:
+            children = {}
+        print(" " * self.print_depth * 4 + f"{key} {name} ({typ}) at {position}")
+        self.print_depth += 1
+        for child_key, child_data in children.items():
+            self._print_reduced_struct_item(child_key, child_data)
         self.print_depth -= 1
 
     def parse_tooltip(self, tooltip):
@@ -250,7 +312,13 @@ class UIAnalyzer(QtWidgets.QMainWindow):
             text = text.strip()  # Remove leading and trailing whitespace
             if len(text) > 0:
                 tooltip_data.append(text)
-        tooltip_data = [tooltip_data[0], "\n\n".join(tooltip_data[1:])]
+        tooltip_data = [
+            tooltip_data[0].replace(r"&lt;", r"<").replace(r"&gt;", r">"),
+            "  ".join(tooltip_data[1:])
+            .replace(r"&lt;", r"<")
+            .replace(r"&gt;", r">")
+            .replace("&quot;", '"'),
+        ]
         return tooltip_data
 
     def generate_markdown(self):
@@ -261,31 +329,100 @@ class UIAnalyzer(QtWidgets.QMainWindow):
         str
             A string containing markdown text that can be included into the main documentation.
         """
-        self.currently_in_groupboxes = []
-        struct = self.export_structure()
-        markdown = self._generate_item_markdown(struct)
-        return markdown
+        struct = self.reduced_structure()
+        markdown_text, markdown_figures = self._generate_item_markdown(struct)
+        return f"---\nnumbering:\n  figure: false\n---\n# {self.name}\n\n"+markdown_text + "\n\n" + markdown_figures
 
-    def _generate_item_markdown(self, struct):
-        this_markdown = ""
+    def _generate_item_markdown(self, reduced_structure):
+        this_text_markdown = ""
+        this_figure_markdown = ""
 
-        if isinstance(struct["widget"], QtWidgets.QGroupBox):
-            this_markdown = (
-                this_markdown + f'\nIn the {struct["widget"].title()} section of the window:\n'
-            )
+        for name, struct in reduced_structure.items():
 
-        if struct["tooltip"] is not None and struct["tooltip"].strip() != "":
-            label, message = self.parse_tooltip(struct["tooltip"])
-            this_markdown = this_markdown + f"\n* **{label}** {message}"
+            if isinstance(struct["widget"], QtWidgets.QGroupBox):
+                figure_file_name = self.name + "__" + struct["name"] + ".png"
+                figure_full_path = os.path.join(
+                    "mdbook", "src", "_generated", "figures", figure_file_name
+                )
+                figure_rel_path = os.path.join("figures", figure_file_name)
+                figure_ref_name = "fig:" + self.name + ":" + struct["name"]
+                px = self.generate_documentation_figure(struct["widget"])
+                px.save(figure_full_path)
+                
+                block_label = "sec:" + self.name + ":" + struct["name"]
+                this_text_markdown = this_text_markdown + f"\n\n({block_label})="
+                this_figure_markdown += f"\n\n:::{{figure}} {figure_rel_path}\n:label: {figure_ref_name}\n {name} Settings\n:::"
 
-        # Go through its children
-        for child in struct["children"]:
-            this_markdown = this_markdown + self._generate_item_markdown(child)
+            if struct["tooltip"] is not None and struct["tooltip"].strip() != "":
+                # This means we would like to build documentation with this widget
+                figure_file_name = self.name + "__" + struct["name"] + ".png"
+                figure_full_path = os.path.join(
+                    "mdbook", "src", "_generated", "figures", figure_file_name
+                )
+                figure_rel_path = os.path.join("figures", figure_file_name)
+                figure_ref_name = "fig:" + self.name + ":" + struct["name"]
+                label, message = self.parse_tooltip(struct["tooltip"])
+                px = self.generate_documentation_figure(struct["widget"])
+                px.save(figure_full_path)
 
-        if isinstance(struct["widget"], QtWidgets.QGroupBox):
-            this_markdown = this_markdown + "\n"
+                this_figure_markdown += f"\n\n:::{{figure}} {figure_rel_path}\n:label: {figure_ref_name}\n **{label}** {message}\n:::"
+                this_text_markdown = (
+                    this_text_markdown + f"\n* [**{label}**](#{figure_ref_name}) {message}"
+                )
 
-        return this_markdown
+            # Go through its children
+            if "children" in struct:
+                child_text, child_figure = self._generate_item_markdown(struct["children"])
+                this_text_markdown = this_text_markdown + child_text
+                this_figure_markdown = this_figure_markdown + child_figure
+
+            if isinstance(struct["widget"], QtWidgets.QGroupBox):
+                this_text_markdown = this_text_markdown + "\n"
+
+        return this_text_markdown, this_figure_markdown
+
+    def generate_documentation_figure(self, widget, padding=100, box_thickness=2, box_padding=5):
+        item_rect = widget.rect()
+        parent_window = widget.window()
+        position = widget.mapTo(parent_window, item_rect.topLeft())
+        height = item_rect.height()
+        width = item_rect.width()
+        widget_rect = QRect(position.x(), position.y(), width, height)
+        window_pixmap = parent_window.grab()
+
+        box_rect = QRect(
+            widget_rect.left() - box_padding,
+            widget_rect.top() - box_padding,
+            widget_rect.width() + 2 * box_padding,
+            widget_rect.height() + 2 * box_padding,
+        )
+
+        # Draw a red box around the widget's geometry within the cropped image
+        painter = QPainter(window_pixmap)
+        pen = QPen(QColor("red"))
+        pen.setWidth(box_thickness)
+        painter.setPen(pen)
+
+        # Draw the rectangle
+        painter.drawRect(box_rect)
+        painter.end()
+
+        # return window_pixmap
+        # Calculate the expanded rectangle around the widget
+        expanded_rect = QRect(
+            widget_rect.left() - padding,
+            widget_rect.top() - padding,
+            widget_rect.width() + 2 * padding,
+            widget_rect.height() + 2 * padding,
+        )
+
+        # Ensure the expanded rectangle stays within the bounds of the window
+        expanded_rect = expanded_rect.intersected(parent_window.rect())
+
+        # Crop the pixmap to the expanded rectangle
+        cropped_pixmap = window_pixmap.copy(expanded_rect)
+
+        return cropped_pixmap
 
 
 app = QtWidgets.QApplication(sys.argv)
